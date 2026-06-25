e086f848fab85898bc6c3d951c088cd3ecf63792
FlyingRC-Official <118458039+FlyingRC-Official@users.noreply.github.com>
2026-06-25 20:57:33 +0800

    Add F4Wing Mini ArduCopter 4.6.3 firmware

--- data/products.js
+++ data/products.js
@@ -1361,6 +1361,7 @@ const PUBLIC_REFERENCE_FILES = {
   "f4wing-mini-mk1": [
     download("assets/downloads/firmware/f4wing-mini-mk1/inav_9.0.1_FLYINGRCF4WINGMINI.hex", "INAV 9.0.1 firmware", "INAV 9.0.1 固件"),
     download("assets/downloads/firmware/f4wing-mini-mk1/AP463_FlyingRCF4WingMini_arduplane_with_bl.hex", "ArduPlane 4.6.3 firmware with bootloader", "ArduPlane 4.6.3 带 Bootloader 固件"),
+    download("assets/downloads/firmware/f4wing-mini-mk1/AP463_FlyingRCF4WingMini_arducopter_with_bl.hex", "ArduCopter 4.6.3 firmware with bootloader", "ArduCopter 4.6.3 带 Bootloader 固件"),
     download("assets/downloads/firmware/f4wing-mini-mk1/betaflight_2025.12.4_STM32F405_FLYINGRCF4WINGMINI.hex", "Betaflight 2025.12.4 firmware", "Betaflight 2025.12.4 固件")
   ],
   "f4wse-pro": [
